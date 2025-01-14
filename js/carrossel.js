7b15a92ed1ab58096459d529c634efc84977f74e
Viviane Novo <viviane.novo.702@ufrn.edu.br>
2025-01-14 03:05:55 -0300

    feat: ajustes na estrutura e na estilização

--- js/carrossel.js
+++
@@ -1,8 +0,0 @@
-let currentIndex = 0;
-
-function moveSlide(direction) {
-  const carouselInner = document.querySelector('.carousel-inner');
-  const items = document.querySelectorAll('.carousel-item');
-  currentIndex = (currentIndex + direction + items.length) % items.length;
-  carouselInner.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
